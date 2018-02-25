49163e071b6ff6c3287490d50dee4aa9e50dd222
micahcourey <micahjcourey@gmail.com>
2018-02-24 18:22:12 -0800

    Work on about page desktop layout

--- frontend/src/app/story-footer/story-footer.component.ts
+++ frontend/src/app/story-footer/story-footer.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'story-footer',
@@ -6,8 +6,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./story-footer.component.scss']
 })
 export class StoryFooterComponent implements OnInit {
+  @Input() fullwidth: boolean;
 
-  constructor() { }
+  constructor() {
+    this.fullwidth = false;
+  }
 
   ngOnInit() {
   }
